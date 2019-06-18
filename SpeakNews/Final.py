740e25b0142e111c4d7b7ed162b251b42ec1b7b0
Joubert Joe Vieira Lellis <joubert.lellis@gmail.com>
2019-06-17 22:31:48 -0300

    Ended
    All thinks fully functional
    this shit now is completely woriking
    great job to myself

--- SpeakNews/Final.py
+++ SpeakNews/Final.py
@@ -5,15 +5,45 @@ import time
 from bs4 import BeautifulSoup
 import requests
 import speech_recognition as sr
+#from subprocess import call
+r = sr.Recognizer()
 
-recon = sr.Recognizer()
-with sr.Microphone() as fonte:
-    audio = recon.listen(fonte)
-    resposta = recon.recognize_google(audio, language='pt')
+
+
+def reconhece():
+    # Reconhece é uma função que serve para
+    #reconhecer os comandos de voz passados
+    #pelo usuário.
+    print("fale agora")
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    try:
+        reconhecido = r.recognize_google(audio, language='pt')
+        print("#      -" + reconhecido)
+        return str(reconhecido).lower()
+    except sr.UnknownValueError:
+        print("Não consegui te entender")
+        reconhece()
+
+def fala(texto):
+    # Inicia-se a rotina de sintetização e reprodução
+    voz = gTTS(texto, lang='pt')
+    voz.save('fala.mp3')
+    mixer.init()
+    mixer.music.load('fala.mp3')
+    mixer.music.get_busy()
+    mixer.music.play()
+    while mixer.music.get_busy():
+        print('Continua falando...')
+        time.sleep(1)
+
+    #call(['espeak','-v','mb-br4','-s','140','-p','60', texto])
+    print(texto)
 
 
 # Inicia rotina do programa
 def init():
+    fala("Bem vindo ao Speak News, escolha uma opção: 1 categorias, 2 pesquisa direta, 3 notícias hoje")
     print("|-------------------------------")
     print("|---- S P E A K   N E W S  -----")
     print("|-------------------------------")
@@ -22,28 +52,34 @@ def init():
     print("#    2 - Pesquisa Direta       #")
     print("#    3 - Notícias Hoje         #")
     print("################################")
+
     print("\n")
     # Pegando o Input do Usuário
-    opcao = input("#    - Categoria: ")
+    print("#    - Escolha: ")
+
+    opcao = reconhece()
+
     # chamando a opção correspondente
-    if (int(opcao) > 0) & (int(opcao) < 4):  # conferindo se o input faz sentido
-        # chamada do Menu.
-        menu(int(opcao))
-    else:
-        # caso o input não faça sentido
-        init()
-    pass
+    menu(opcao)
+    print(opcao)
 
 
 # Menu de funções
 def menu(opcaoEscolhida):
-    if (opcaoEscolhida == 1):
+    if (opcaoEscolhida == "1") or (opcaoEscolhida == "categorias"):
+        fala("opção escolhida foi Categorias")
+
         categoria()
-    elif (opcaoEscolhida == 2):
+    elif (opcaoEscolhida == "2")or (opcaoEscolhida == "pesquisa direta"):
+        fala("opção escolhida foi pesquisa direta")
+
         urlPesquisa("")
-    elif (opcaoEscolhida == 3):
+    elif (opcaoEscolhida == "3")or (opcaoEscolhida == "notícias hoje"):
+        fala("opção escolhida foi Notícias hoje")
+
         pesquisa("https://news.google.com/?hl=pt-BR&gl=BR&ceid=BR:pt-419")
-    pass
+    else:
+        init()
 
 
 # Categorias para facilitar pesquisa do usuário
@@ -56,17 +92,26 @@ def categoria():
     print("|---     0 - Outra ")
     print("|-------------------------------------------|")
     # recebendo Categoria
-    escolhido = input("|---       - Categoria: ")
-    if( (escolhido == "1") or (escolhido.lower() == "esporte" )):
+    print("|---       - Categoria: ")
+    fala("Escolha uma categoria, 1 esporte, 2 jogos, 3 política, 4 tv, 0 outras")
+
+    escolhido = reconhece()
+    if( (escolhido == "1") or (escolhido == "esporte" )):
+        fala("Categoria Esporte")
         urlPesquisa("Esporte")
-    elif ((escolhido == "2") or (escolhido.lower() == "jogos" )):
+    elif ((escolhido == "2") or (escolhido == "jogos" )):
+        fala("Categoria Jogos")
         urlPesquisa("Jogos")
-    elif ((escolhido == "3") or (escolhido.lower() == "política" )):
+    elif ((escolhido == "3") or (escolhido == "política" )):
         urlPesquisa("Política")
-    elif ((escolhido == "4")  or (escolhido.lower() == "tv" )):
+        fala("Categoria Política")
+    elif ((escolhido == "4")  or (escolhido == "tv" )):
+        fala("Categoria TV")
         urlPesquisa("Tv")
-    elif ((escolhido == "0") or (escolhido.lower() == "outro" )):
-        urlPesquisa(input("Categoria: "))
+    elif ((escolhido == "0") or (escolhido == "outro" )):
+        fala("Diga a outra categoria a ser usada:")
+
+        urlPesquisa(reconhece())
 
 
 
@@ -76,8 +121,10 @@ def categoria():
 def urlPesquisa(prePesquisa):
 
     # Colhendo a pesquisa do Usuário
-    print("|---- Digite sua pesquia: ")
-    search = input("|---- ")
+    print("|---- Fale sua pesquia: ")
+    fala("Diga sua pesquisa")
+
+    search = reconhece()
     # preparando a URL para fazer a pesquisa
     url = 'https://news.google.com/search?q=' + prePesquisa + search + '&hl=pt-BR&gl=BR&ceid=BR%3Apt-419'
     # Trocar os espaços em branco por %20 (que é usado pelo google para representar o mesmo)
@@ -144,14 +191,16 @@ def escolhePesquisa(nLink, link_array):
         time.sleep(1)
 
     # Opções de escolha da Notícia
+    fala("Escolha uma opção, 1 ouvir a pesquisa atual, 2 ouvir a próxima notícia, 3 cancelar sua pesquisa")
     print("################################")
     print("#    1 - Ler Pesquisa          #")
     print("#    2 - Próxima Pesquisa      #")
     print("#    3 - Cancelar Pesquisa     #")
     print("################################")
-    option = input("#      - ")
 
-    if (option == "1"):
+    option = reconhece()
+
+    if (option == "1") or (option == "ler pesquisa"):
         # Caso escolha ouvir a notícia
         # Inicia-se a rotina de sintetização e reprodução
         voz = gTTS(a.text, lang='pt')
@@ -164,17 +213,18 @@ def escolhePesquisa(nLink, link_array):
             print('Continua falando...')
             time.sleep(1)
 
-    elif (option == "2"):
+    elif (option == "2") or (option == "próxima pesquisa"):
         # Pula para o próximo Link da lista
         print(nLink)
         escolhePesquisa(nLink + 2,link_array)
-    elif (option == "3"):
+    elif (option == "3") or (option == "cancelar"):
         # Recomeça o programa
         init()
     else:
         # Caso o input não seja reconhecido da forma esperada.
         print("#    -Repita por Favor")
-        option = input("#      - ")
+        fala("Repita por favor")
+        option = reconhece()
 
 
 # Inicia a cadeia de execução.
